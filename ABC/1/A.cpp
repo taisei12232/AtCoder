56f7226de449ccceb4e3650b2a4e38f9e090070a
taisei12232 <taisei12232@gmail.com>
2022-07-02 23:44:03 +0900

    ABC1 A AC

---
+++ ABC/1/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,N) for(int i = 0;i < (int)N;++i)
+#define rep1(i,N) for(int i = 1;i < (int)N;++i)
+#define repi(i,j,N) for(int j = i;j < (int)N;++j)
+#define co(x) cout << x << endl
+#define cosp(x) cout << x << " "
+#define sort(A) sort(A.begin(),A.end())
+#define reverse(A) reverse(A.begin(),A.end())
+#define INF 1e18
+using ll = long long;
+using ull = unsigned long long;
+int main(){
+int a,b;
+cin >> a >> b;
+co(a-b);
+}
